38faa4d9de94f83c9872cf79671729701bed4e09
Ivan Muzzolini <ivan.muzzolini@gmail.com>
2020-01-31 18:24:55 +0100

    move executables function

--- ports/osg/portfile.cmake
+++ ports/osg/portfile.cmake
@@ -2,45 +2,26 @@ include(vcpkg_common_functions)
 
 set(OSG_VERSION "3.6.4")
 
-# TODO: create functions "move_items_with_pdb"
 # TODO: create functions to clean empty folders
 
-function(move_items)
-    cmake_parse_arguments(move "" "GLOB;FROM;TO" "" ${ARGN})
+function(move_executables)
+    cmake_parse_arguments(move "" "FROM;TO" "" ${ARGN})
 
-    if(move_GLOB)
-        file(MAKE_DIRECTORY ${move_TO})
-        file(GLOB ITEMS_TO_MOVE ${move_FROM}/${move_GLOB})
-        foreach(ITEM ${ITEMS_TO_MOVE})
-            if(${ITEM} MATCHES ".*\.exe" OR ${ITEM} MATCHES ".*\.dll")
-                get_filename_component(BASENAME ${ITEM} NAME_WLE)
-                get_filename_component(FOLDER ${ITEM} DIRECTORY)
-                set(PDB ${FOLDER}/${BASENAME}.pdb)
-            endif()
-            if(move_TO)
-                file(COPY ${ITEM} DESTINATION ${move_TO})
-                if(PDB AND EXISTS ${PDB})
-                    file(COPY ${PDB} DESTINATION ${move_TO})
-                endif()
-            endif()
-            file(REMOVE_RECURSE ${ITEM})
-            if(PDB AND EXISTS ${PDB})
-                file(REMOVE_RECURSE ${PDB})
-            endif()
-        endforeach()
-        file(GLOB REMAINING_ITEMS ${move_FROM}/*.*)
-        if(${REMAINING_ITEMS} STREQUAL "")
-            file(REMOVE_RECURSE ${move_FROM})
+    file(MAKE_DIRECTORY ${move_TO})
+    file(GLOB ITEMS_TO_MOVE ${move_FROM}/*.exe)
+    foreach(ITEM ${ITEMS_TO_MOVE})
+        get_filename_component(BASENAME ${ITEM} NAME_WLE)
+        get_filename_component(FOLDER ${ITEM} DIRECTORY)
+        set(PDB ${FOLDER}/${BASENAME}.pdb)
+        file(COPY ${ITEM} DESTINATION ${move_TO})
+        if(PDB AND EXISTS ${PDB})
+            file(COPY ${PDB} DESTINATION ${move_TO})
         endif()
-    else()
-        if(EXISTS ${move_FROM})
-            if(move_TO)
-                file(RENAME ${move_FROM} ${move_TO})
-            else()
-                file(REMOVE_RECURSE ${move_FROM})
-            endif()
+        file(REMOVE_RECURSE ${ITEM})
+        if(PDB AND EXISTS ${PDB})
+            file(REMOVE_RECURSE ${PDB})
         endif()
-    endif()
+    endforeach()
 endfunction()
 
 vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
@@ -66,36 +47,23 @@ vcpkg_install_cmake()
 
 if(VCPKG_TARGET_IS_WINDOWS)
     if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
-        move_items(
-            GLOB *.exe
+        move_executables(
             FROM ${CURRENT_PACKAGES_DIR}/bin
             TO ${CURRENT_PACKAGES_DIR}/tools/osg
         )
-        move_items(
-            GLOB *.dll
-            FROM ${CURRENT_PACKAGES_DIR}/bin/osgPlugins-${OSG_VERSION}
-            TO ${CURRENT_PACKAGES_DIR}/tools/osg/osgPlugins-${OSG_VERSION}
-        )
-        move_items(
-            FROM ${CURRENT_PACKAGES_DIR}/debug/include
-        )
+        file(RENAME ${CURRENT_PACKAGES_DIR}/bin/osgPlugins-${OSG_VERSION} ${CURRENT_PACKAGES_DIR}/tools/osg/osgPlugins-${OSG_VERSION})
+        if(EXISTS ${CURRENT_PACKAGES_DIR}/debug/include)
+            file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
+        endif()
     endif()
     if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
-        move_items(
-            GLOB *.exe
+        move_executables(
             FROM ${CURRENT_PACKAGES_DIR}/debug/bin
             TO ${CURRENT_PACKAGES_DIR}/debug/tools/osg
         )
-        move_items(
-            GLOB *.dll
-            FROM ${CURRENT_PACKAGES_DIR}/debug/bin/osgPlugins-${OSG_VERSION}
-            TO ${CURRENT_PACKAGES_DIR}/debug/tools/osg/osgPlugins-${OSG_VERSION}
-        )
+        file(RENAME ${CURRENT_PACKAGES_DIR}/debug/bin/osgPlugins-${OSG_VERSION} ${CURRENT_PACKAGES_DIR}/debug/tools/osg/osgPlugins-${OSG_VERSION})
         if(VCPKG_BUILD_TYPE STREQUAL "debug")
-            move_items(
-                FROM ${CURRENT_PACKAGES_DIR}/debug/include
-                TO ${CURRENT_PACKAGES_DIR}/include
-            )
+            file(RENAME ${CURRENT_PACKAGES_DIR}/debug/include ${CURRENT_PACKAGES_DIR}/include)
         endif()
     endif()
 endif()
