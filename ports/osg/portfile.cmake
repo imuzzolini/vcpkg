8ca535fece5f08bfafd60f1600a72e0f5a6a6236
Ivan Muzzolini <ivan.muzzolini@gmail.com>
2020-01-31 18:09:28 +0100

    simplifying finishing folders

--- ports/osg/portfile.cmake
+++ ports/osg/portfile.cmake
@@ -1,22 +1,57 @@
 include(vcpkg_common_functions)
 
+set(OSG_VERSION "3.6.4")
+
+function(move_items)
+    cmake_parse_arguments(move "" "GLOB;FROM;TO" "" ${ARGN})
+
+    if(move_GLOB)
+        file(MAKE_DIRECTORY ${move_TO})
+        file(GLOB ITEMS_TO_MOVE ${move_FROM}/${move_GLOB})
+        foreach(ITEM ${ITEMS_TO_MOVE})
+            if(${ITEM} MATCHES ".*\.exe" OR ${ITEM} MATCHES ".*\.dll")
+                get_filename_component(BASENAME ${ITEM} NAME_WLE)
+                get_filename_component(FOLDER ${ITEM} DIRECTORY)
+                set(PDB ${FOLDER}/${BASENAME}.pdb)
+            endif()
+            if(move_TO)
+                file(COPY ${ITEM} DESTINATION ${move_TO})
+                if(PDB AND EXISTS ${PDB})
+                    file(COPY ${PDB} DESTINATION ${move_TO})
+                endif()
+            endif()
+            file(REMOVE_RECURSE ${ITEM})
+            if(PDB AND EXISTS ${PDB})
+                file(REMOVE_RECURSE ${PDB})
+            endif()
+        endforeach()
+        file(GLOB REMAINING_ITEMS ${move_FROM}/*.*)
+        if(${REMAINING_ITEMS} STREQUAL "")
+            file(REMOVE_RECURSE ${move_FROM})
+        endif()
+    else()
+        if(EXISTS ${move_FROM})
+            if(move_TO)
+                file(RENAME ${move_FROM} ${move_TO})
+            else()
+                file(REMOVE_RECURSE ${move_FROM})
+            endif()
+        endif()
+    endif()
+endfunction()
+
 vcpkg_check_linkage(ONLY_DYNAMIC_LIBRARY)
 
 vcpkg_from_github(
-	OUT_SOURCE_PATH SOURCE_PATH
-	REPO openscenegraph/OpenSceneGraph
-	REF OpenSceneGraph-3.6.4
-	SHA512 7cb34fc279ba62a7d7177d3f065f845c28255688bd29026ffb305346e1bb2e515a22144df233e8a7246ed392044ee3e8b74e51bf655282d33ab27dcaf12f4b19
-	HEAD_REF master
+    OUT_SOURCE_PATH SOURCE_PATH
+    REPO openscenegraph/OpenSceneGraph
+    REF OpenSceneGraph-${OSG_VERSION}
+    SHA512 7cb34fc279ba62a7d7177d3f065f845c28255688bd29026ffb305346e1bb2e515a22144df233e8a7246ed392044ee3e8b74e51bf655282d33ab27dcaf12f4b19
+    HEAD_REF master
     PATCHES
         collada.patch
 )
 
-if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
-    set(OSG_DYNAMIC OFF)
-else()
-    set(OSG_DYNAMIC ON)
-endif()
 file(REMOVE ${SOURCE_PATH}/CMakeModules/FindSDL2.cmake)
 vcpkg_configure_cmake(
     SOURCE_PATH ${SOURCE_PATH}
@@ -26,33 +61,42 @@ vcpkg_configure_cmake(
 
 vcpkg_install_cmake()
 
-# handle osg tools and plugins
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/include)
-
-set(OSG_TOOL_PATH ${CURRENT_PACKAGES_DIR}/tools/osg)
-file(MAKE_DIRECTORY ${OSG_TOOL_PATH})
-
-file(GLOB OSG_TOOLS ${CURRENT_PACKAGES_DIR}/bin/*.exe)
-file(COPY ${OSG_TOOLS} DESTINATION ${OSG_TOOL_PATH})
-file(REMOVE_RECURSE ${OSG_TOOLS})
-file(GLOB OSG_TOOLS_DBG ${CURRENT_PACKAGES_DIR}/debug/bin/*.exe)
-file(REMOVE_RECURSE ${OSG_TOOLS_DBG})
-
-file(GLOB OSG_PLUGINS_DBG ${CURRENT_PACKAGES_DIR}/debug/bin/osgPlugins-3.6.4/*.dll)
-file(COPY ${OSG_PLUGINS_DBG} DESTINATION ${CURRENT_PACKAGES_DIR}/debug/tools/osg/osgPlugins-3.6.4)
-file(GLOB OSG_PLUGINS_REL ${CURRENT_PACKAGES_DIR}/bin/osgPlugins-3.6.4/*.dll)
-file(COPY ${OSG_PLUGINS_REL} DESTINATION ${OSG_TOOL_PATH}/osgPlugins-3.6.4)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/bin/osgPlugins-3.6.4/)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin/osgPlugins-3.6.4/)
+if(VCPKG_TARGET_IS_WINDOWS)
+    if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "release")
+        move_items(
+            GLOB *.exe
+            FROM ${CURRENT_PACKAGES_DIR}/bin
+            TO ${CURRENT_PACKAGES_DIR}/tools/osg
+        )
+        move_items(
+            GLOB *.dll
+            FROM ${CURRENT_PACKAGES_DIR}/bin/osgPlugins-${OSG_VERSION}
+            TO ${CURRENT_PACKAGES_DIR}/tools/osg/osgPlugins-${OSG_VERSION}
+        )
+        move_items(
+            FROM ${CURRENT_PACKAGES_DIR}/debug/include
+        )
+    endif()
+    if(NOT DEFINED VCPKG_BUILD_TYPE OR VCPKG_BUILD_TYPE STREQUAL "debug")
+        move_items(
+            GLOB *.exe
+            FROM ${CURRENT_PACKAGES_DIR}/debug/bin
+            TO ${CURRENT_PACKAGES_DIR}/debug/tools/osg
+        )
+        move_items(
+            GLOB *.dll
+            FROM ${CURRENT_PACKAGES_DIR}/debug/bin/osgPlugins-${OSG_VERSION}
+            TO ${CURRENT_PACKAGES_DIR}/debug/tools/osg/osgPlugins-${OSG_VERSION}
+        )
+        if(VCPKG_BUILD_TYPE STREQUAL "debug")
+            move_items(
+                FROM ${CURRENT_PACKAGES_DIR}/debug/include
+                TO ${CURRENT_PACKAGES_DIR}/include
+            )
+        endif()
+    endif()
+endif()
 
 # Handle copyright
 file(COPY ${SOURCE_PATH}/LICENSE.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/osg)
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/osg/LICENSE.txt ${CURRENT_PACKAGES_DIR}/share/osg/copyright)
-
-#Cleanup
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/debug/share)
-file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib/pkgconfig ${CURRENT_PACKAGES_DIR}/debug/lib/pkgconfig)
-
-if(VCPKG_LIBRARY_LINKAGE STREQUAL static)
-    file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/bin ${CURRENT_PACKAGES_DIR}/debug/bin)
-endif()
