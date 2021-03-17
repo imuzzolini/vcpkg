15ecfbb0e56e32dc6919b4efe48b816595e1c4dc
Ivan Muzzolini <ivan.muzzolini@gmail.com>
2021-03-17 14:52:40 +0000

    re-added logics to clean debug/include useless folder

--- ports/fann/portfile.cmake
+++ ports/fann/portfile.cmake
@@ -41,6 +41,8 @@ if(NOT VCPKG_CMAKE_SYSTEM_NAME)
     file(REMOVE_RECURSE ${CURRENT_PACKAGES_DIR}/lib/pkgconfig)
 endif()
 
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
 # Handle copyright
 file(COPY ${SOURCE_PATH}/COPYING.txt DESTINATION ${CURRENT_PACKAGES_DIR}/share/${PORT})
 file(RENAME ${CURRENT_PACKAGES_DIR}/share/${PORT}/COPYING.txt ${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright)
